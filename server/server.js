f6295f2864d298c49d70e917166f8ad9a071a231
RubyK31 <rubykala.webethics@gmail.com>
2025-02-12 11:20:11 +0530

    webhooks updates

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
-const app = express()
+const app = express();
 
-await connectDB()
+await connectDB();
 
-app.use(cors())
-app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => res.send('API working'))
-app.post('/clerk', clerkWebhooks)
+app.get("/", (req, res) => res.send("API working"));
+app.post("/clerk", express.json(), clerkWebhooks);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('Server started on port  '+ PORT)
-})
+  console.log("Server started on port  " + PORT);
+});
